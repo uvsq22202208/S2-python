0093587ce0e6e81e787615434c89a3d0aa7b60eb
Adrien CLAIMAND <adrien.claimand@ens.uvsq.fr>
2023-03-21 10:20:25 +0100

    commit depuis VScode

--- Projet/projet_mastermind.py
+++ Projet/projet_mastermind.py
@@ -1,21 +1,21 @@
 import tkinter as tk
 
-8 couleurs:
-bleu (1)
-vert (2)
-rouge (3)
-violet (4)
-jaune (5)
-orange (6)
-blanc (7)
-noir (8)
+#8 couleurs:
+#bleu (1)
+#vert (2)
+#rouge (3)
+#violet (4)
+#jaune (5)
+#orange (6)
+#blanc (7)
+#noir (8)
 
 dico_couleurs = {'bleu': 1 , 'vert': 2, 'rouge': 3, 'violet': 4 , 'jaune': 5 , 'orange': 6 , 'blanc': 7 , 'noir': 8}
 
 
-4 pions composant le code 
+#4 pions composant le code 
 
-10 essais
+#10 essais
 
 def placement_pion (l, l1):
     for i in range(len(l)):
@@ -23,3 +23,6 @@ def placement_pion (l, l1):
             print ("bien placé")
         elif (l1[i] in l) and (l1[i] != l[i]):
             print ("mal placé")
+
+
+hftrdtyughuui
